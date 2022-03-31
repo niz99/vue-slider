fbf67845b4b3b3a3014d3f408b5288a000caa65d
Nizar Fergag <fergag99@gmail.com>
2022-03-31 17:36:18 +0200

    setInterval settato

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ var app = new Vue(
         el: '#root',
         data: {
             array: [
-                'https://source.unsplash.com/random',
-                'https://source.unsplash.com/random',
-                'https://source.unsplash.com/random',
+                'https://source.unsplash.com/random/?shoes',
+                'https://source.unsplash.com/random/?fruits',
+                'https://source.unsplash.com/random/?city',
                 'https://source.unsplash.com/random'
             ],
             index: 0
@@ -13,7 +13,20 @@ var app = new Vue(
         methods: {
             avanti: function() {
                 this.index = (this.index+1)%this.array.length
+            },
+            indietro: function() {
+                if(this.index == 0){
+                    this.index = (this.array.length - 1)
+                } else {
+                    this.index--
+                }
             }
+        },
+        mounted: function() {
+            setInterval(() => {
+                this.index = (this.index+1)%this.array.length
+                //console.log(this.index)
+            }, 3000)
         }
     }
 )
